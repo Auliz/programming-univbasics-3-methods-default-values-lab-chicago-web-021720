5f5500537ebcbc806743fc35525d507311e52060
Joseph E Auz <joseph.auz77@gmail.com>
2020-02-07 10:47:25 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -5,5 +5,5 @@ def meal_choice(veg1, veg2, protein = "meat")
   nutritious_meal = "What a nutritious meal!" 
   food_ordered = "A plate of #{protein} with #{veg1} and #{veg2}."
   puts nutritious_meal
-  return food_ordered
+  puts food_ordered
 end 
